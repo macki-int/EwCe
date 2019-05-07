7b1e86e6313220531a0a393259ed66c6aae7f078
mjanicki <macki@int.pl>
2019-05-07 15:10:03 +0200

    init repo

---
+++ src/main/java/pl/mj/EwCe/EwCeApplication.java
@@ -0,0 +1,13 @@
+package pl.mj.EwCe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwCeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwCeApplication.class, args);
+	}
+
+}
