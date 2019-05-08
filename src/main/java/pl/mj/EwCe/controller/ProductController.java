2a404c392627fad0d553f4c24f96d03d7fd1517d
macki-int <macki@int.pl>
2019-05-09 00:08:51 +0200

    added dev/prod properties

---
+++ src/main/java/pl/mj/EwCe/controller/ProductController.java
@@ -0,0 +1,9 @@
+package pl.mj.EwCe.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+}
